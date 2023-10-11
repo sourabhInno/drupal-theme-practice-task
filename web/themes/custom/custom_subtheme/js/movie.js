edc651fd8151681d7cf7017cb3e37fc81c73336f
sourabhInno <sourabh.sisodia@innoraft.com>
2023-10-11 17:33:51 +0530

    Changes recommended by mentor

--- web/themes/custom/custom_subtheme/js/movie.js
+++ web/themes/custom/custom_subtheme/js/movie.js
@@ -1,13 +1,14 @@
 (function ($, Drupal, once) {
   Drupal.behaviors.moviePage = {
     attach: function (context, settings) {
-      let modalDiv = document.getElementById("expandedContainer");
+      let modalDiv = document.querySelector(".expandedContainer");
       let closeTag = document.getElementById("close-tag");
       let expandedImage = document.getElementById("expandedImage");
       let movieTitle = document.getElementById("movieTitle");
       let modalInfo = document.getElementById("modalInfo");
+
       once("slide", ".slider", context).forEach((element) => {
-        $(".slider").slick({
+        $(".slider .view-content").slick({
           arrows: true,
           centerPadding: "0px",
           dots: true,
@@ -15,28 +16,31 @@
           infinite: false,
         });
       });
-      once("popUp", ".imageContainer", context).forEach((element) => {
-        const imagesContainer = document.querySelectorAll(".imageContainer");
+      once("popUp", ".view-id-movies_list", context).forEach((element) => {
+        const imagesContainer = document.querySelector(".view-id-movies_list");
         let handleImageClick = (e) => {
-          let element = e.target.parentNode;
-          let title = element.querySelector("h1");
-          let date = element.querySelector("p");
-          let image = element.querySelector("img");
-          let body = element.querySelector("h3");
-          image = image.src;
-          title = title.textContent;
-          date = date.textContent;
-          body = body.textContent;
-          modalDiv.style.display = "block";
-          expandedImage.src = image.src;
-          modalDiv.style.display = "block";
-          movieTitle.textContent = title;
-          expandedImage.src = image;
-          modalInfo.textContent = body;
+          if (e.target.tagName === "IMG") {
+            let element = e.target.parentNode;
+            let title = element.querySelector("h1");
+            let date = element.querySelector("p");
+            let image = element.querySelector("img");
+            let body = element.querySelector("h3");
+            image = image.src;
+            title = title.textContent;
+            date = date.textContent;
+            body = body.textContent;
+            modalDiv.style.display = "block";
+            expandedImage.src = image;
+            modalDiv.style.display = "block";
+            movieTitle.textContent = title;
+            expandedImage.src = image;
+            modalInfo.textContent = body;
+          } else {
+            return;
+          }
         };
-        imagesContainer.forEach((imageContainer) => {
-          imageContainer.onclick = handleImageClick;
-        });
+
+        imagesContainer.onclick = handleImageClick;
       });
       once("popUp", ".expanded", context).forEach((element) => {
         let handleCloseClick = () => {
